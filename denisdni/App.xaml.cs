246c0f505e5c715f8eddd190ab93723432f431e8
opilane <opilane@HP-CZC6297B9B>
2020-10-30 11:53:17 +0200

    Done2x

--- denisdni/App.xaml.cs
+++ denisdni/App.xaml.cs
@@ -10,7 +10,7 @@ namespace denisdni
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
